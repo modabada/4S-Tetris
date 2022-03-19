741cf6a03b179a833c37c3fa8e4a2560974d2645
modabada <miw1102@naver.com>
2022-03-20 04:27:14 +0900

    Adding system variable
    add width, height, speed, score, board

--- Assets/CS/GameManager.cs
+++ Assets/CS/GameManager.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager: MonoBehaviour {
+    public static readonly int width = 10;
+    public static readonly int height = 30;
+    public static double speed = 0.8;
+    public static double score = 0;
+    public static Transform[,,] board = new Transform[width, height, width];    // board[x, y, z]
     private void Awake() {
         DontDestroyOnLoad(this);
     }
 
     public void StartGame() {
-        Debug.Log("Start Game");
         SceneManager.LoadScene(1);
     }
 
     public void ExitGame() {
-        Debug.Log("Exit Game");
+        SceneManager.LoadScene(2);
     }
 }
