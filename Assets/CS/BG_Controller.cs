6d6914ffae3a194c46d0094ed8c7c3829729c64b
modabada <miw1102@naver.com>
2022-03-20 04:32:31 +0900

    simple formatting

--- Assets/CS/BG_Controller.cs
+++ Assets/CS/BG_Controller.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BG_Controller : MonoBehaviour
-{
+public class BG_Controller: MonoBehaviour {
     private void Awake() {
         DontDestroyOnLoad(this);
     }
